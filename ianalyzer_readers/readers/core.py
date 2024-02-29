45f2154c0e8251202c4e8f95e137f0fb2f8e0066
Luka van der Plas <lukavdplas@mailfence.com>
2024-02-29 19:47:23 +0100

    docstrings for core module

--- ianalyzer_readers/readers/core.py
+++ ianalyzer_readers/readers/core.py
@@ -1,10 +1,15 @@
 '''
-More with core classes
+This module defines the base classes on which all Readers are built.
+
+It implements very little functionality of its own, but defines the interface
+that Readers implement.
+
+The module defines two classes, `Field` and `Reader`.
 '''
 
 from .. import extract
 from datetime import datetime
-
+from typing import List, Iterable, Dict, Any
 import logging
 
 logger = logging.getLogger()
@@ -12,59 +17,99 @@ logger = logging.getLogger()
 
 class Reader(object):
     '''
-    Subclasses of this class define corpora and their documents by specifying:
+    A base class for readers. Readers are objects that can generate documents
+    from a source dataset.
+
+    Subclasses of `Reader` can be created to read particular data formats or even
+    particular datasets.
+    
+    The `Reader` class is not intended to be used directly. Some methods need to
+    be implemented in child components, and will raise `NotImplementedError` if
+    you try to use `Reader` directly.
+
+    A fully implemented `Reader` subclass will define how to read a dataset by
+    describing:
 
     - How to obtain its source files.
-    - What attributes its documents have.
-    - How to extract said attributes from the source files.
-    - What each attribute looks like in terms of the search form.
+    - What fields each document contains.
+    - How to extract said fields from the source files.
     '''
 
     @property
-    def data_directory(self):
+    def data_directory(self) -> str:
         '''
         Path to source data directory.
+
+        Raises:
+            NotImplementedError: This method needs to be implementd on child
+                classes. It will raise an error by default.
         '''
-        raise NotImplementedError('CorpusDefinition missing data_directory')
+        raise NotImplementedError('Reader missing data_directory')
 
 
     @property
-    def fields(self):
+    def fields(self) -> List:
         '''
-        Each corpus should implement a list of fields, that is, instances of
-        the `Field` class, containing information about each attribute.
-        MUST include a field with `name='id'`.
+        The list of fields that are extracted from documents.
+
+        These should be instances of the `Field` class (or implement the same API).
+
+        Raises:
+            NotImplementedError: This method needs to be implementd on child
+                classes. It will raise an error by default.
         '''
-        raise NotImplementedError('CorpusDefinition missing fields')
+        raise NotImplementedError('Reader missing fields')
 
     @property
-    def fieldnames(self):
+    def fieldnames(self) -> List[str]:
+        '''
+        A list containing the name of each field of this Reader
+        '''
         return [field.name for field in self.fields]
 
-    def sources(self, **kwargs):
+    def sources(self, **kwargs) -> Iterable:
         '''
-        Obtain source files for the corpus, relevant to the given timespan.
+        Obtain source files for the Reader.
 
-        Specifically, returns an iterator of tuples that each contain a string
-        filename and a dictionary of associated metadata. The latter is usually
-        empty or contains only a timestamp; but any data that is to be
-        extracted without reading the file itself can be specified there.
+        Returns:
+            an iterable of tuples that each contain a string path, and a dictionary
+                with associated metadata. The metadata can contain any data that was
+                extracted before reading the file itself, such as data based on the
+                file path, or on a metadata file.
+
+        Raises:
+            NotImplementedError: This method needs to be implementd on child
+                classes. It will raise an error by default.
         '''
         raise NotImplementedError('CorpusDefinition missing sources')
 
-    def source2dicts(self, sources):
+    def source2dicts(self, source) -> Iterable[Dict[str, Any]]:
         '''
-        Generate an iterator of document dictionaries from a given source file.
+        Given a source file, returns an iterable of extracted documents.
 
-        The dictionaries are created from this corpus' `Field`s.
+        Returns:
+            an iterable of document dictionaries. Each of these is a dictionary,
+                where the keys are names of this Reader's `fields`, and the values
+                are based on the extractor of each field.
+
+        Raises:
+            NotImplementedError: This method needs to be implementd on child
+                classes. It will raise an error by default.
         '''
         raise NotImplementedError('CorpusDefinition missing source2dicts')
 
-    def documents(self, sources=None):
+    def documents(self, sources:Iterable[str]=None) -> Iterable[Dict[str, Any]]:
         '''
-        Generate an iterator of document dictionaries directly from the source
-        files. The source files are generated by self.sources(); however, if
-        `sources` is specified, those source/metadata tuples are used instead.
+        Returns an iterable of extracted documents from source files.
+
+        Parameters:
+            sources: an iterable of paths to source files. If omitted, the reader
+                class will use the value of `self.sources()` instead.
+
+        Returns:
+            an iterable of document dictionaries. Each of these is a dictionary,
+                where the keys are names of this Reader's `fields`, and the values
+                are based on the extractor of each field.
         '''
         sources = sources or self.sources()
         return (document
@@ -88,11 +133,16 @@ class Reader(object):
 
 class Field(object):
     '''
-    Fields hold the following data:
-    - a short hand name (name), which will be used as its key in the document
-    - how to extract data from the source documents (extractor)
-    - whether this field is required
-    - whether this field should be skipped
+    Fields are the elements of information that you wish to extract from each document.
+
+    Parameters:
+        name:  a short hand name (name), which will be used as its key in the document
+        extractor: an Extractor object that defines how this field's data can be
+            extracted from source documents.
+        required: whether this field is required. The `Reader` class should skip the
+            document is the value for this Field is `None`, though this is not supported
+            for all readers.
+        skip: if `True`, this field will not be included in the results.
     '''
 
     def __init__(self,
@@ -107,57 +157,3 @@ class Field(object):
         self.extractor = extractor
         self.required = required
         self.skip = skip
-
-# Helper functions ############################################################
-
-
-def string_contains(target):
-    '''
-    Return a predicate that performs a case-insensitive search for the target
-    string and returns whether it was found.
-    '''
-    def f(string):
-        return bool(target.lower() in string.lower() if string else False)
-    return f
-
-
-def until(year):
-    '''
-    Returns a predicate to determine from metadata whether its 'date' field
-    represents a date before or on the given year.
-    '''
-    def f(metadata):
-        date = metadata.get('date')
-        return date and date.year <= year
-    return f
-
-
-def after(year):
-    '''
-    Returns a predicate to determine from metadata whether its 'date' field
-    represents a date after the given year.
-    '''
-    def f(metadata):
-        date = metadata.get('date')
-        return date and date.year > year
-    return f
-
-
-def consolidate_start_end_years(start, end, min_date, max_date):
-    ''' given a start and end date provided by the user, make sure
-    - that start is not before end
-    - that start is not before min_date (corpus variable)
-    - that end is not after max_date (corpus variable)
-    '''
-    if isinstance(start, int):
-        start = datetime(year=start, month=1, day=1)
-    if isinstance(end, int):
-        end = datetime(year=end, month=12, day=31)
-    if start > end:
-        tmp = start
-        start = end
-        end = tmp
-    if start < min_date:
-        start = min_date
-    if end > max_date:
-        end = max_date
